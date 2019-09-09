01c91b03f6702b5680c2f26350685fe5e497eb90
Sean Johnson <sean@ttys0.net>
2019-09-09 09:10:11 -0500

    updated Ceph config to be consistent with the rest of conform_config.sh

--- conform_config.sh
+++ conform_config.sh
@@ -393,11 +393,11 @@ set_kernel_config CONFIG_SQUASHFS y
 
 # Ceph support for Block Device (RBD) and Filesystem (FS)
 # https://docs.ceph.com/docs/master/
-set_kernel_config CONFIG_CEPH_LIB=m
-set_kernel_config CONFIG_CEPH_LIB_USE_DNS_RESOLVER=y
-set_kernel_config CONFIG_CEPH_FS=m
-set_kernel_config CONFIG_CEPH_FSCACHE=y
-set_kernel_config CONFIG_CEPH_FS_POSIX_ACL=y
-set_kernel_config CONFIG_BLK_DEV_RBD=m
+set_kernel_config CONFIG_CEPH_LIB m
+set_kernel_config CONFIG_CEPH_LIB_USE_DNS_RESOLVER y
+set_kernel_config CONFIG_CEPH_FS m
+set_kernel_config CONFIG_CEPH_FSCACHE y
+set_kernel_config CONFIG_CEPH_FS_POSIX_ACL y
+set_kernel_config CONFIG_BLK_DEV_RBD m
 
 
